20b67a9c51d87aa917da3502cb2551c634a14316
kinaoshi <67078788+kinaoshi@users.noreply.github.com>
2021-04-20 20:14:09 +0900

    ⚗ クリックイベントの学習記述

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,33 @@
+import { loadGetInitialProps } from "next/dist/next-server/lib/utils";
 import Head from "next/head";
+import { useCallback } from "react";
 import { Footer } from "src/components/Footer";
 import { Header } from "src/components/Header";
 import { Main } from "src/components/Main";
 import styles from "src/styles/Home.module.css";
 
+//function handleClick (e) {
+//const handleClick = (e) =>  {
+  // e.preventDefault();
+  // console.log(e.target.href);
+  // // console.log(e.target);
+  // alert(123);
+//};
+// const handleClick = (e,foo) =>  {
+//   console.log(e.target.href);
+//   e.preventDefault();
+//   alert(foo);
+// };
 export default function Home() {
+
+const foo = 1;
+
+const handleClick = useCallback((e) =>  {
+  console.log(e.target.href);
+  e.preventDefault();
+  alert(foo);
+}, []);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,8 +35,20 @@ export default function Home() {
       </Head>
       <Header />
 
-      <Main page="index" />
+      <a
+        href={"./about"}
+        onClick={handleClick}
+        // onClick={(e)=> handleClick(e, foo)}
 
+        // onClick={(e) => {
+        //     e.preventDefault();
+        //     console.log(e.target.href);
+        // }
+        // }
+      >
+        ボタン
+      </a>
+      <Main page="index" />
       <Footer />
     </div>
   );
