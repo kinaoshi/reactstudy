4e223aa28c20c00c91b86c301995fc8554ba408d
kinaoshi <67078788+kinaoshi@users.noreply.github.com>
2021-05-20 16:13:38 +0900

    ✨ useStateで文字列を真偽値について解説

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,49 +1,57 @@
 import { loadGetInitialProps } from "next/dist/next-server/lib/utils";
 import Head from "next/head";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { Footer } from "src/components/Footer";
 import { Header } from "src/components/Header";
 import { Main } from "src/components/Main";
 import styles from "src/styles/Home.module.css";
 
-// const handleClick = (e,foo) =>  {
-//   console.log(e.target.href);
-//   e.preventDefault();
-//   alert(foo);
-// };
 export default function Home() {
+  const [count, setCount] = useState(1);
+  const [text, setText] = useState("");
+  const [isShow, setIsShow] = useState(true);
 
-const [count, setCount] = useState(1);
+  const handleClick = useCallback(() => {
+   if (count < 10) {
+     setCount((count) => count + 1);
+   }
+  },[count]);
 
+  const handleDisplay = useCallback(() => {
+    setIsShow((isShow) => !isShow);
+  },[]);
 
-const handleClick = (e) =>  {
+  const handleChange = useCallback((e) => {
+    if(e.target.value.length > 5) {
+      alert('5文字以内にして下さい');
+      return;
+    }
+    setText(e.target.value.trim());
+  },[]);
 
-setCount(count => count + 1);
-setCount(count => count + 1);
-};
-useEffect(() => {
-
-document.body.style.backgroundColor = 'lightblue';
-
-return() => {
-
-document.body.style.backgroundColor = '';
-
-}
-},[]);
-
-
-
+  useEffect(() => {
+    document.body.style.backgroundColor = "lightblue";
+    return () => {
+      document.body.style.backgroundColor = "";
+    };
+  }, []);
+  
   return (
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
       </Head>
       <Header />
-      <h1>{count}</h1>
-      <button onClick={handleClick}>
-        ボタン
-      </button>
+      {isShow ? <h1>{count}</h1> : null}
+      <button onClick={handleClick}>ボタン</button>
+      <button onClick={handleDisplay}>
+        { isShow ? '非表示' : '表示'}
+        </button>
+      <input
+        type="text"
+        value={text}
+        onChange={handleChange}
+      />
       <Main page="index" />
       <Footer />
     </div>
