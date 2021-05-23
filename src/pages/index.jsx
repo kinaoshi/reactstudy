4c720a5857a05417a29ea70230e2545a72309d76
kinaoshi <67078788+kinaoshi@users.noreply.github.com>
2021-05-23 21:33:53 +0900

    ✨ useStateで配列を扱ってみた

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -10,11 +10,12 @@ export default function Home() {
   const [count, setCount] = useState(1);
   const [text, setText] = useState("");
   const [isShow, setIsShow] = useState(true);
+  const [array, setArray] = useState([]);
 
   const handleClick = useCallback(() => {
-   if (count < 10) {
+    if (count < 10) {
       setCount((prevCount) => prevCount + 1);
-   }
+    }
   }, [count]);
 
   const handleDisplay = useCallback(() => {
@@ -22,12 +23,21 @@ export default function Home() {
   }, []);
 
   const handleChange = useCallback((e) => {
-    if(e.target.value.length > 5) {
-      alert('5文字以内にして下さい');
+    if (e.target.value.length > 5) {
+      alert("5文字以内にして下さい");
       return;
     }
     setText(e.target.value.trim());
-  },[]);
+  }, []);
+  const handleAdd=useCallback(()=>{
+    setArray((prevArray)=> {
+      if(prevArray.some((item)=>item===text)){
+        alert("同じ要素が既に存在します");
+        return prevArray;
+      }
+        return [...prevArray,text];
+    })
+  },[text]);
 
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
@@ -35,7 +45,7 @@ export default function Home() {
       document.body.style.backgroundColor = "";
     };
   }, []);
-  
+
   return (
     <div className={styles.container}>
       <Head>
@@ -44,14 +54,14 @@ export default function Home() {
       <Header />
       {isShow ? <h1>{count}</h1> : null}
       <button onClick={handleClick}>ボタン</button>
-      <button onClick={handleDisplay}>
-        { isShow ? '非表示' : '表示'}
-        </button>
-      <input
-        type="text"
-        value={text}
-        onChange={handleChange}
-      />
+      <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
+      <input type="text" value={text} onChange={handleChange} />
+      <button onClick = {handleAdd}>追加</button>
+      <ul>
+        {array.map((item) => {
+          return <li key={item}>{item}</li>;
+        })}
+      </ul>
       <Main page="index" />
       <Footer />
     </div>
