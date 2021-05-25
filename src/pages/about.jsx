4ef8621f2f9baa19234e1d154f778185cfdfe3b6
kinaoshi <67078788+kinaoshi@users.noreply.github.com>
2021-05-25 21:40:23 +0900

    ✨ CustomHooksを使ってうまくまとめました

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -5,13 +5,40 @@ import { Header } from "src/components/Header";
 import { Main } from "src/components/Main";
 import styles from "src/styles/Home.module.css";
 
-export default function Home() {
+import { useBgLightBlue } from "src/hooks/useBgLightBlue";
+import { useCounter } from "src/hooks/useCounter";
+
+import { useCallback, useState } from "react";
+import { useInputArray } from "src/hooks/useInputArray";
+
+
+
+
+
+
+
+export default function About() {
+  const { count, isShow, handleClick, handleDisplay } = useCounter();
+  const { text, array, handleChange, handleAdd } = useInputArray();
+  useBgLightBlue();
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Create Next</title>
       </Head>
       <Header />
+      {isShow ? <h1>{count}</h1> : null}
+      <button onClick={handleClick}>ボタン</button>
+      <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
+
+      <input type="text" value={text} onChange={handleChange} />
+      <button onClick={handleAdd}>追加</button>
+      <ul>
+        {array.map((item) => {
+          return <li key={item}>{item}</li>;
+        })}
+      </ul>
 
       <Main page="about" />
 
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,49 +1,28 @@
-import { loadGetInitialProps } from "next/dist/next-server/lib/utils";
+
 import Head from "next/head";
-import { useCallback, useEffect, useState } from "react";
+import {useCallback, useState } from "react";
 import { Footer } from "src/components/Footer";
 import { Header } from "src/components/Header";
 import { Main } from "src/components/Main";
+import { useBgLightBlue } from "src/hooks/useBgLightBlue";
+import { useCounter } from "src/hooks/useCounter";
+import { useInputArray } from "src/hooks/useInputArray";
 import styles from "src/styles/Home.module.css";
 
+
+
+
+
 export default function Home() {
-  const [count, setCount] = useState(1);
-  const [text, setText] = useState("");
-  const [isShow, setIsShow] = useState(true);
-  const [array, setArray] = useState([]);
+  const { count, isShow, handleClick, handleDisplay } = useCounter();
+  const { text, array, handleChange, handleAdd } = useInputArray();
+  useBgLightBlue();
 
   const handleOnClick=()=>{
     alert("次は15から")
-    }
-  }, [count]);
+  }
 
-  const handleDisplay = useCallback(() => {
-    setIsShow((prevIsShow) => !prevIsShow);
-  }, []);
 
-  const handleChange = useCallback((e) => {
-    if (e.target.value.length > 5) {
-      alert("5文字以内にして下さい");
-      return;
-    }
-    setText(e.target.value.trim());
-  }, []);
-  const handleAdd=useCallback(()=>{
-    setArray((prevArray)=> {
-      if(prevArray.some((item)=>item===text)){
-        alert("同じ要素が既に存在します");
-        return prevArray;
-      }
-        return [...prevArray,text];
-    })
-  },[text]);
-
-  useEffect(() => {
-    document.body.style.backgroundColor = "lightblue";
-    return () => {
-      document.body.style.backgroundColor = "";
-    };
-  }, []);
 
   return (
     <div className={styles.container}>
@@ -51,11 +30,13 @@ export default function Home() {
         <title>Create Next App</title>
       </Head>
       <Header />
+
       {isShow ? <h1>{count}</h1> : null}
       <button onClick={handleClick}>ボタン</button>
       <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
+
       <input type="text" value={text} onChange={handleChange} />
-      <button onClick = {handleAdd}>追加</button>
+      <button onClick={handleAdd}>追加</button>
       <ul>
         {array.map((item) => {
           return <li key={item}>{item}</li>;
@@ -68,3 +49,6 @@ export default function Home() {
     </div>
   );
 }
+
+
+
