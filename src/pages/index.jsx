d622220ea3e16ac8cd0a8f6fdbf2881ea60656e4
kinaoshi <67078788+kinaoshi@users.noreply.github.com>
2021-07-01 15:19:59 +0900

    ✨ APIを叩こう

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,19 @@
 import Head from "next/head";
-import { Footer } from "src/components/Footer";
+import { useCallback, useEffect, useState } from "react";
 import { Header } from "src/components/Header";
-import { Main } from "src/components/Main";
 import styles from "src/styles/Home.module.css";
 
- const Home =(props) => {
+const Home = (props) => {
+  const [posts, setPosts] = useState([]);
+
+  const getPosts = useCallback(async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const json = await res.json();
+    setPosts(json);
+  }, []);
+  useEffect(() => {
+    getPosts();
+  }, [getPosts]);
 
   return (
     <div className={styles.container}>
@@ -12,22 +21,15 @@ import styles from "src/styles/Home.module.css";
         <title>Create Next App</title>
       </Head>
       <Header />
+      {posts.length > 0 ? (
 
-      {props.isShow ? <h1>{props.count}</h1> : null}
-      <button onClick={props.handleClick}>ボタン</button>
-      <button onClick={props.handleDisplay}>{props.isShow ? "非表示" : "表示"}</button>
-
-      <input type="text" value={props.text} onChange={props.handleChange} />
-      <button onClick={props.handleAdd}>追加</button>
-      <ul>
-        {props.array.map((item) => {
-          return <li key={item}>{item}</li>;
+      <ol>
+        {posts.map((post) => {
+          return <li key={post.id}>{post.title}</li>;
         })}
-      </ul>
-
-      <Main page="index" />
-      <Footer />
+      </ol>
+      ) : null };
     </div>
   );
-}
+};
 export default Home;
