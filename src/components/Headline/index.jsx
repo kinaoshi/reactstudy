0aee44a83c5a3b1c73357e178d396f3c76e0d5be
kinaoshi <67078788+kinaoshi@users.noreply.github.com>
2021-07-23 22:06:40 +0900

    🚚 ファイルシステムルーティングを試しました。

--- src/components/Headline/index.jsx
+++ src/components/Headline/index.jsx
@@ -2,7 +2,7 @@ import classes from "src/components/Headline/Headline.module.css";
 
 export const Headline = (props) => {
   const handleOnClick = () => {
-    alert("次は#18から");
+    alert("次は#22から");
   };
   return (
     <div>
