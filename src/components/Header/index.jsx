c35f21b29659ea2cfa62e924cee9a8bb8ab6c9f2
kinaoshi <67078788+kinaoshi@users.noreply.github.com>
2021-08-03 15:16:36 +0900

    🚚 パスの変更

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 import classes from 'src/components/Header/Header.module.css'
 
 const NAV_ITEMS = [
+  {href: "/", label: "Index"},
   {href: "/posts", label: "Posts"},
-  {href: "/about", label: "About"},
 ]
 
 export const Header = () => {
