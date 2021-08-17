29d0f17907d76b98676ad435c38192182bf0e881
kinaoshi <67078788+kinaoshi@users.noreply.github.com>
2021-08-17 14:46:13 +0900

    🚚 英語の間違い修正、複数形を単数形に

--- src/hooks/useFetchArray.jsx
+++ src/hooks/useFetchArray.jsx
@@ -13,23 +13,31 @@ import useSWR from "swr";
 };
 
 const API_URL = "https://jsonplaceholder.typicode.com";
+// post
+
+export const usePosts = () => {
+  return useFetchArray(`${API_URL}/posts`);
+};
+export const usePostsByUserId = (id) => {
+  return useFetchArray(id? `${API_URL}/posts?userId=${id}`:null);
+ }
+//  users
+
+ export const useUsers = () => {
+   return useFetchArray(`${API_URL}/users`);
+ };
+
+//  comments
 
 export const useComments = () => {
   return useFetchArray(`${API_URL}/comments`);
 };
-export const usePosts = () => {
-  return useFetchArray(`${API_URL}/posts`);
-};
-export const useUsers = () => {
-  return useFetchArray(`${API_URL}/users`);
-};
 
-export const useCommentsByPostsId = (id) => {
+
+export const useCommentsByPostId = (id) => {
  return useFetchArray(id? `${API_URL}/comments?postId=${id}`:null);
 }
 
-export const usePostsByUserId = (id) => {
- return useFetchArray(id? `${API_URL}/posts?userId=${id}`:null);
-}
+
 
 
