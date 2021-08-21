57c355baea75aba17189a22c22e90e90bca413ba
kinaoshi <ku.928.maid@gmail.com>
2021-08-21 20:33:15 +0900

    ➕ Tailwind CSS用のセットアップ

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,18 +1,19 @@
-import "src/styles/globals.css";
+import "tailwindcss/tailwind.css";
+// import "src/styles/globals.css";
 import Head from "next/head";
 import { Layout } from "../components/Layout";
 
 const MyApp = ({ Component, pageProps }) => {
-  return (
-    <>
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Layout>
+	return (
+		<>
+			<Head>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<Layout>
         <Component {...pageProps} />
-      </Layout>
-    </>
-  );
+			</Layout>
+		</>
+	);
 };
 
-export default MyApp
+export default MyApp;
