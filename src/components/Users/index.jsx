4a5406ed10459e93559bcebaa1b3e6d886c0fd58
kinaoshi <ku.928.maido@gmail.com>
2021-08-26 16:16:05 +0900

    ♻️ #29途中まで

--- src/components/Users/index.jsx
+++ src/components/Users/index.jsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import { useUsers } from "src/hooks/useFetchArray";
 
-
 export const UsersComponent = () => {
   const { data, error, isLoading, isEmpty } = useUsers();
 
@@ -15,16 +14,19 @@ export const UsersComponent = () => {
     return <p>No users found</p>;
   }
   return (
-    <ol>
+    <ul className="grid grid-cols-3 gap-4">
       {data.map((user) => {
         return (
           <li key={user.id}>
             <Link href={`/users/${user.id}`}>
-              <a>{`${user.name} (${user.email})`}</a>
+              <a className="block p-2 shadow rounded hover:bg-gray-100">
+                <h1 className="font-bold">{user.name}</h1>
+                <div className="text-sm">{user.email}</div>
+              </a>
             </Link>
           </li>
         );
       })}
-    </ol>
+    </ul>
   );
 };
