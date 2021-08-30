f369083a54149cadbd7afcb8a71a65c55ae1ccfc
kinaoshi <ku.928.maid@gmail.com>
2021-08-30 21:23:22 +0900

    ✅ SWRの更新と最適化

--- src/hooks/usePost.jsx
+++ src/hooks/usePost.jsx
@@ -1,5 +1,6 @@
 import useSWR from "swr";
 export const usePost = (id) => {
+  const fetcher = (...args) => fetch(...args).then(res => res.json())
   const { data, error } = useSWR(
     id
       ? `https://jsonplaceholder.typicode.com/posts/${id}`
--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,17 +1,24 @@
 import "tailwindcss/tailwind.css";
-// import "src/styles/globals.css";
+
 import Head from "next/head";
 import { Layout } from "../components/Layout";
+import { SWRConfig } from "swr";
 
 const MyApp = ({ Component, pageProps }) => {
 	return (
 		<>
+		<SWRConfig 
+      value={{
+        fetcher: (resource, init) => fetch(resource, init).then(res => res.json())
+      }}
+    >
 			<Head>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<Layout>
         <Component {...pageProps} />
 			</Layout>
+			</SWRConfig>
 		</>
 	);
 };
