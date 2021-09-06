113308bf52e54b3574e889b68a678b251cfc9df2
kinaoshi <ku.928.maid@gmail.com>
2021-09-06 19:22:30 +0900

    ✨ ユーザー詳細ぺーじでSSRを実践

--- src/pages/posts/[id]/index.jsx
+++ src/pages/posts/[id]/index.jsx
@@ -1,9 +1,11 @@
 import { Header } from "src/components/Header";
 import { Post } from "src/components/Post";
 
+
 const PostsId = () => {
   return (
     <div>
+      
       <Header />
       <Post />
     </div>
--- src/pages/users/[id]/index.jsx
+++ src/pages/users/[id]/index.jsx
@@ -1,17 +1,30 @@
 import { Header } from "src/components/Header";
 import { UserComponent } from "src/components/User";
+import { SWRConfig } from "swr";
 
+export const getServerSideProps = async (ctx) => {
+	const { id } = ctx.query;
+	const API_URL = `https://jsonplaceholder.typicode.com/users/${id}`;
+	const user = await fetch(API_URL);
+	const userData = await user.json();
 
+	return {
+		props: {
+			fallback: {
+				[API_URL]: userData,
+			},
+		},
+	};
+};
 
-
-const UsersId = () => {
-
-  return (
-    <div>
-      <Header />
-     <UserComponent/>
-    </div>
-  );
+const UsersId = (props) => {
+	const { fallback } = props;
+	return (
+		<SWRConfig value={{ fallback }}>
+			<Header />
+			<UserComponent />
+		</SWRConfig>
+	);
 };
 
 export default UsersId;
