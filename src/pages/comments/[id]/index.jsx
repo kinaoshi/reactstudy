42a62ccfd4dc77ce21d42b301f1fa8c696369f08
kinaoshi <ku.928.maid@gmail.com>
2021-10-01 15:48:53 +0900

    🔥_limitfallback:false,trueの学習

--- src/pages/comments/[id]/index.jsx
+++ src/pages/comments/[id]/index.jsx
@@ -1,46 +1,57 @@
+import { useRouter } from "next/router";
+import { comment } from "postcss";
 import { CommentComponent } from "src/components/Comment";
 import { Header } from "src/components/Header";
 import { SWRConfig } from "swr";
 
 export const getStaticPaths = async () => {
-  const comments = await fetch("https://jsonplaceholder.typicode.com/comments");
-  const commentsData = await comments.json();
-  const paths = commentsData.map((comment) => ({
-    params: { id: comment.id.toString() },
-  }));
+	const comments = await fetch(
+		"https://jsonplaceholder.typicode.com/comments?_limit=10"
+	);
+	const commentsData = await comments.json();
+	const paths = commentsData.map((comment) => ({
+		params: { id: comment.id.toString() },
+	}));
 
-  return {
-    paths,
-    fallback: false,
-  };
+	return {
+		paths,
+		fallback: "blocking",
+	};
 };
 
 export const getStaticProps = async (ctx) => {
-  const { id } = ctx.params;
-  const COMMENT_API_URL = `https://jsonplaceholder.typicode.com/comments/${id}`;
-  const comment = await fetch(COMMENT_API_URL);
-  const commentData = await comment.json();
+	const { id } = ctx.params;
+	const COMMENT_API_URL = `https://jsonplaceholder.typicode.com/comments/${id}`;
+	const comment = await fetch(COMMENT_API_URL);
 
-  return {
-    props: {
-      fallback: {
-        [COMMENT_API_URL]: commentData,
-      },
-    },
-  };
+	if (!comment.ok) {
+		return {
+			notFound: true,
+		};
+	}
+
+	const commentData = await comment.json();
+
+	return {
+		props: {
+			fallback: {
+				[COMMENT_API_URL]: commentData,
+			},
+		},
+	};
 };
 
 const CommentsId = (props) => {
-  const { fallback } = props;
+	const { fallback } = props;
 
-  return (
-    <div>
-      <Header />
-      <SWRConfig value={{ fallback }}>
-        <CommentComponent />
-      </SWRConfig>
-    </div>
-  );
+	return (
+		<div>
+			<Header />
+			<SWRConfig value={{ fallback }}>
+				<CommentComponent />
+			</SWRConfig>
+		</div>
+	);
 };
 
 export default CommentsId;
